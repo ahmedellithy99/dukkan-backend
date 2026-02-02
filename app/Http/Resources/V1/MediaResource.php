1900b29d27eae4c8b46badf2f16ca8f544370ec3
ahmedellithy99 <ahmedelithy99@gmail.com>
2026-02-02 18:44:03 +0200

    refactor(media): add general MediaResource to the app

---
+++ app/Http/Resources/V1/MediaResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MediaResource extends JsonResource
+{
+    /**
+     * Transform the media resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'file_name' => $this->file_name,
+            'mime_type' => $this->mime_type,
+            'size' => $this->size,
+            'url' => $this->getUrl(),
+            'thumb_url' => $this->hasGeneratedConversion('thumb') 
+                ? $this->getUrl('thumb') 
+                : $this->getUrl(),
+        ];
+    }
+}
